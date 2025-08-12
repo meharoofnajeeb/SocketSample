1ff92111de593f81582a380eeeb90be1a433607f
Meharoof Najeeb <meharoofnajeeb@gmail.com>
2025-08-12 14:19:30 -0400

    Initial Commit

---
+++ SocketSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocketSample
+//
+//  Created by Meharoof Najeeb on 2025-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocketSample/SocketSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketSampleApp.swift
+//  SocketSample
+//
+//  Created by Meharoof Najeeb on 2025-08-12.
+//
+
+import SwiftUI
+
+@main
+struct SocketSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
